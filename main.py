b3daedc1fc35e7f2cb3c2260787048f178cc0ba4
ShadowOfear <florentinboisson@yahoo.fr>
2016-03-30 17:41:38 +0200

    mise ne place de la page web

--- main.py
+++ main.py
@@ -3,5 +3,29 @@ import time
 # import get_json_files
 # import database
 
-creation_table_database()
-recuperation_json()
+#creation_table_database()
+#recuperation_json()
+
+from bottle import get, post, request, run
+
+@get('/login') # or @route('/login')
+def login():
+    return '''
+        <form action="/login" method="post">
+            Username: <input name="username" type="text" />
+            Password: <input name="password" type="password" />
+            <input value="Login" type="submit" />
+        </form>
+    '''
+
+@post('/login') # or @route('/login', method='POST')
+def do_login():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+    if check_login(username, password):
+        return "<p>Your login information was correct.</p>"
+    else:
+        return "<p>Login failed.</p>"
+
+
+run(host="localhost", port=8080)
