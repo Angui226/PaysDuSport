35f496bf0a4dd70c83322b5d738ac34db9b20313
Angui <enguerand.boisliveau@gmail.com>
2016-03-30 14:47:34 +0200

    création classe installation

--- installation_class.py
+++ installation_class.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python3
+
 class Installation:
     def __init__(self,json_obj):
-        pass
+        self.latitude=json_obj['Latitude']
+        self.longitude=json_obj['Longitude']
+        self.numero_install=json_obj['InsNumeroInstall']
+        self.cp=json_obj['InsCodePostal']
+        self.commune=json_obj['ComLib']
+        self.rue=json_obj['InsLibelleVoie']
+        self.train=json_obj['InsTransportTrain']
+        self.bus=json_obj['InsTransportBus']
+        self.tram=json_obj['InsTransportTram']
