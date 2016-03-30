ff78cf79245d6ff768397bb6b7ee5824b5304f04
ShadowOfear <florentinboisson@yahoo.fr>
2016-03-30 15:21:04 +0200

    creation objet equipement

--- equipement_class.py
+++ equipement_class.py
@@ -1,3 +1,6 @@
 class Equipement:
     def __init__(self,json_obj):
-        pass
+        self.nature_libelle = json_obj['NatureLibelle']
+        self.ins_nom = json_obj['InsNom']
+        self.ins_numero_install = json_obj['InsNumeroInstall']
+        self.equipement_id = json_obj['EquipementId']
