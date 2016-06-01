a16e47d6a8717cfebfebf4c5f63da5715ff6f19d
Angui <enguerand.boisliveau@gmail.com>
2016-06-01 15:34:56 +0200

    read me

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-Pays du sport
+#Pays du sport
 
+##What is it ?
+It's an application that allows the user to search the sports practiced in a town. A click on an activity load all the detail about the installation where it takes place.
+Only avaible for the french region Pays de la Loire
 
-launch the programm : python3 /src/main.py
+## How to to launch it
+- put yourself to the directory src
+- execute main.py -> python3 main.py
+- open http://localhost:8080/ on a browser
+
+##Who made it ?
+
+Two french student in second year or IT : BOISLIVEAU Enguérand & BOISSON Florentin
--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+"""
+main function. Receive requests, return template and data from the database
+"""
 import sqlite3 #Database management system
 import time
 import urllib #recuperer corps requete http
@@ -13,27 +15,37 @@ from database import *
 
 from bottle import get, post, request, run, template, route, static_file,response
 
+
 @route('/static/<filename>', name='static')
 def server_static(filename):
+    """
+    function that redirect all files (css and js) to the directory static
+    """
     return static_file(filename, root='static')
 
 
 #Home_page
 @get('/') # or @route('/')
 def home():
+    """
+    return the home page with the list of the activities and the towns in the db
+    """
     list_activities = get_list_activites()
     list_town = get_list_town()
     return template('home', list_activities = list_activities, list_town = list_town )
 
 @post('/') # or @route('/', method='POST')
 def do_home():
+    """
+    return a template with the list of the request (town and sport given by the user)
+    """
     r = urllib.parse.unquote(request.body.read().decode())
     r = r.replace("+", " ")
-    
+
     r = r.split("&")
     tab1 = r[0].split("=")
     tab2 = r[1].split("=")
-    
+
     if (tab1[0] == "town"):
         town = tab1[1]
         sport =tab2[1]
@@ -62,6 +74,9 @@ def do_home():
 
 @post('/change') # or @route('/', method='POST')
 def do_changeTown():
+    """
+    refresh the list of the sport according to the selected town
+    """
     r = urllib.parse.unquote(request.body.read().decode())
     pattern = "town="
     town_i = r.index(pattern)
@@ -77,6 +92,9 @@ def do_changeTown():
 #Detail page
 @get('/detail/<id_installation>')
 def detail(id_installation):
+    """
+    return all the detail of an installation with its id as argument
+    """
     specific_installation = get_specific_installation( id_installation )
     #faire requete avec tous les equipements ayant cette installation
 
